2806c0e1406f49b39ad512e877b9116e7f9b5317
Roland O. Aifuwa <siroapple10@gmail.com>
2020-10-28 18:11:42 -0500

    server with post and get

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-const https = require("https");
+
 
 const app = express();
-
-app.use(express.static("public"));
+var items=["Buy food"];
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    var today = new Date();
+    var currDay = today.getDay();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", { kindOfDay: day, newListItem: items}); // using EJS to pass day to list
+
+
 });
-app.listen(process.env.PORT|| 3000, function (req, res) {
+app.post("/", function (req, res) {
+    var item=req.body.newItem;
+     items.push(item);
+    res.redirect("/");
+
+});
+app.listen(3000, function (req, res) {
 
     console.log("server is running on port 3000");
 });
